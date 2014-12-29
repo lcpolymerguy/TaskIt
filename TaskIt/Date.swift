3e90712d6c9e345830882fad518270376a461642
Morgan Hart <morganhart@Morgans-MacBook-Pro.local>
2014-12-29 15:24:01 -0500

    First commit

---
+++ TaskIt/Date.swift
@@ -0,0 +1,9 @@
+//
+//  Date.swift
+//  TaskIt
+//
+//  Created by Morgan Hart on 12/29/14.
+//  Copyright (c) 2014 Morgan Hart. All rights reserved.
+//
+
+import Foundation
---
+++ TaskIt/TaskModel.swift
@@ -0,0 +1,9 @@
+//
+//  TaskModel.swift
+//  TaskIt
+//
+//  Created by Morgan Hart on 12/15/14.
+//  Copyright (c) 2014 Morgan Hart. All rights reserved.
+//
+
+import Foundation
